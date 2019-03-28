cb5f9d44d1a8773ef2c285d1d8fe92d423a0aafa
Karabaglidis <Karabaglidis@KOL06WSTHV-7315>
2019-03-28 18:53:55 +0100

    added myspotify mvc project

---
+++ src/Web/MySpotifyMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySpotifyMVC.Models;
+using Tasprof.Apps.MySpotifyDroid.Services.Spotify;
+
+namespace MySpotifyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ISpotifyService _spotifyService;
+
+        public HomeController(ISpotifyService spotifyService)
+        {
+            _spotifyService = spotifyService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var playllists = await _spotifyService.GetPlaylists();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
