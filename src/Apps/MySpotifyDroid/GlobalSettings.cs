4117025a7a6233dd2ea19eef1d1665821087f10d
Karabaglidis <Karabaglidis@KOL06WSTHV-7315>
2019-03-29 13:59:26 +0100

    subcommit

--- src/Apps/MySpotifyDroid/GlobalSettings.cs
+++ src/Apps/MySpotifyDroid/GlobalSettings.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Tasprof.Apps.MySpotifyDroid.Helpers;
+﻿using Tasprof.Apps.MySpotifyDroid.Helpers;
 
 namespace Tasprof.Apps.MySpotifyDroid
 {
