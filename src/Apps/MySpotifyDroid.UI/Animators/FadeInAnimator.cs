36f348ce51d414a2b3da506eea9f4c81c0c06f33
Karabaglidis <Karabaglidis@KOL06WSTHV-7315>
2019-04-03 17:40:50 +0200

    added animation for play history item

---
+++ src/Apps/MySpotifyDroid.UI/Animators/FadeInAnimator.cs
@@ -0,0 +1,58 @@
+﻿using Android.Animation;
+using Android.Content;
+using Android.Support.V7.Widget;
+using Android.Views.Animations;
+
+namespace Tasprof.Apps.MySpotifyDroid.UI.Animators
+{
+    public class FadeInAnimator : SimpleItemAnimator
+    {
+        Context context;
+        public override bool IsRunning => false;
+
+
+        public FadeInAnimator(Context context)
+        {
+            this.context = context;
+        }
+
+        public override bool AnimateAdd(RecyclerView.ViewHolder holder)
+        {
+            Animator set = AnimatorInflater.LoadAnimator(context, Resource.Animation.playhistoryitem_fade_in);
+            set.SetInterpolator(new BounceInterpolator());
+            set.SetTarget(holder.ItemView);
+            set.Start();
+            return true;
+        }
+
+        public override bool AnimateChange(RecyclerView.ViewHolder oldHolder, RecyclerView.ViewHolder newHolder, int fromLeft, int fromTop, int toLeft, int toTop)
+        {
+            return false;
+        }
+
+        public override bool AnimateMove(RecyclerView.ViewHolder holder, int fromX, int fromY, int toX, int toY)
+        {
+            return false;
+        }
+
+        public override bool AnimateRemove(RecyclerView.ViewHolder holder)
+        {
+            return false;
+        }
+
+        public override void EndAnimation(RecyclerView.ViewHolder item)
+        {
+           
+        }
+
+        public override void EndAnimations()
+        {
+           
+        }
+
+        public override void RunPendingAnimations()
+        {
+           
+        }
+    }
+}
--- src/Apps/MySpotifyDroid.UI/Views/PlayHistoryView.cs
+++ src/Apps/MySpotifyDroid.UI/Views/PlayHistoryView.cs
@@ -1,9 +1,9 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.Widget;
-using Android.Views.Animations;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Droid.Support.V7.RecyclerView;
+using Tasprof.Apps.MySpotifyDroid.UI.Animators;
 using Tasprof.Apps.MySpotifyDroid.ViewModels;
 
 namespace Tasprof.Apps.MySpotifyDroid.UI.Views
@@ -19,12 +19,7 @@ namespace Tasprof.Apps.MySpotifyDroid.UI.Views
             var recyclerView = FindViewById<MvxRecyclerView>(Resource.Id.mvxRvPlayHistory);
             LinearLayoutManager layoutmanager = (LinearLayoutManager)recyclerView.GetLayoutManager();
             recyclerView.AddOnScrollListener(new MvxRecyclerViewOnScrollListener(layoutmanager, ViewModel));
-            //var context = recyclerView.Context;
-            //LayoutAnimationController controller;
-            //controller = AnimationUtils.LoadLayoutAnimation(context, Resource.Animation.abc_fade_in);
-            //recyclerView.LayoutAnimation = controller;
-            //recyclerView.ScheduleLayoutAnimation();
-
+            recyclerView.SetItemAnimator(new FadeInAnimator(recyclerView.Context));
         }
     }
 
@@ -58,5 +53,4 @@ namespace Tasprof.Apps.MySpotifyDroid.UI.Views
         }
 
     }
-
 }
